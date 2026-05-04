fae35c3fdf1556d774f41955ec19a5f45d78188c
Rizwan Arshad <153654644+rizwanarshad042@users.noreply.github.com>
2026-05-04 20:06:21 +0500

    .

--- frontend_app/distance_calc_frontend/android/app/build.gradle.kts
+++ frontend_app/distance_calc_frontend/android/app/build.gradle.kts
@@ -24,7 +24,8 @@ android {
         applicationId = "com.example.distance_calc_frontend"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        // tflite_flutter requires minSdk 26; override the Flutter variable here.
+        minSdk = 26
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
